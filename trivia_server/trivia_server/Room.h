ea4da2f88e190719239a56e3979371eee4524d19
Eddet Moshe <edeet@themoshes.com>
2022-05-01 23:04:40 +0300

    Created class Room

---
+++ trivia_server/trivia_server/Room.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "LoggedUser.h"
+#include <vector>
+
+typedef struct RoomData
+{
+	unsigned int id;
+	std::string name;
+	unsigned int maxPlayers;
+	unsigned int numOfQuestionsInGame;
+	unsigned int timePerQuestion;
+	unsigned int isActive;
+}RoomData;
+
+class Room
+{
+public:
+	Room(LoggedUser user, RoomData data);
+	void addUser(LoggedUser user);
+	void removeUser(LoggedUser user);
+	std::vector<std::string> getAllUsers();
+
+private:
+	RoomData m_metadata;
+	std::vector<LoggedUser> m_users;
+};
+
