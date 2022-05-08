40b96e72ef5c871e15efe1316003770e99467501
Eddet Moshe <edeet@themoshes.com>
2022-05-08 11:41:35 +0300

    cleaned the code

--- trivia_server/trivia_server/MenuRequestHandler.h
+++ trivia_server/trivia_server/MenuRequestHandler.h
@@ -15,7 +15,7 @@ public:
 	virtual RequestResult handleRequest(RequestInfo r);
 
 private:
-	RequestResult getRooms(RequestInfo r); //TODO: get also room id
+	RequestResult getRooms(RequestInfo r);
 	RequestResult getPlayesrInRoom(RequestInfo r);
 	RequestResult joinRoom(RequestInfo r);
 	RequestResult createRoom(RequestInfo r);
