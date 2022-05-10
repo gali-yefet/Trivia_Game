aa45289b29e9cf39a235d7c2e27ae18670d9a693
Gali Yefet <yefetg1967@gmail.com>
2022-05-10 10:11:21 +0300

    finished statistics gui

---
+++ trivia_client/trivia_client/LeaderBoard.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace trivia_client
+{
+    /// <summary>
+    /// Interaction logic for LeaderBoard.xaml
+    /// </summary>
+    public partial class LeaderBoard : Window
+    {
+        public LeaderBoard()
+        {
+            InitializeComponent();
+            //-->
+            //ServiceReference1.ImojWCFServiceClient client = new ServiceReference1.ImojWCFServiceClient();
+            //listView1.Items.Clear();
+            //var userList = client.getUsers();
+            //listView1.ItemsSource = userList;
+        }
+    }
+}
