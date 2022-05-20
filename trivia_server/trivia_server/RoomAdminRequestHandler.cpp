f64a06295cdd51d2f7519d995d1c74eba589da1b
Eddet Moshe <edeet@themoshes.com>
2022-05-20 18:01:32 +0300

    created getRoomState

--- trivia_server/trivia_server/RoomAdminRequestHandler.cpp
+++ trivia_server/trivia_server/RoomAdminRequestHandler.cpp
@@ -1,4 +1,6 @@
 #include "RoomAdminRequestHandler.h"
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h"
 
 RoomAdminRequestHandler::RoomAdminRequestHandler(int roomId, std::string username, RoomManager& roomManager, RequestHandlerFactory& handlerFactory):
     m_room(roomManager.getRoom(roomId)), m_user(LoggedUser(username)), m_roomManager(roomManager), m_handlerFactory(handlerFactory) {
@@ -11,5 +13,19 @@ bool RoomAdminRequestHandler::isRequestRelevant(RequestInfo r)
 
 RequestResult RoomAdminRequestHandler::handleRequest(RequestInfo r)
 {
+    m_room = m_roomManager.getRoom(m_room.getRoomData().id); //refresh the room
+
     return RequestResult(); //TODO
+}
+
+RequestResult RoomAdminRequestHandler::getRoomState(RequestInfo r)
+{
+    GetRoomStateResponse response;
+    response.answerTimeout = m_room.getRoomData().timePerQuestion;
+    response.hasGameBegun = m_room.getRoomData().isActive == IN_GAME;
+    response.players = m_room.getAllUsers();
+    response.questionCount = m_room.getRoomData().numOfQuestionsInGame;
+    response.status = r.requestCode;
+    std::vector<unsigned char> buffer = JsonResponsePacketSerializer::serializeGetRoomStateResponse(response);
+    return IRequestHandler::createRequestResult(buffer, this);
 }
--- trivia_server/trivia_server/RoomMemberRequestHandler.cpp
+++ trivia_server/trivia_server/RoomMemberRequestHandler.cpp
@@ -1,4 +1,6 @@
 #include "RoomMemberRequestHandler.h"
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h"
 
 RoomMemberRequestHandler::RoomMemberRequestHandler(int roomId, std::string username, RoomManager& roomManager, RequestHandlerFactory& handlerFactory):
     m_room(roomManager.getRoom(roomId)), m_user(LoggedUser(username)), m_roomManager(roomManager), m_handlerFactory(handlerFactory)
@@ -12,5 +14,19 @@ bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo r)
 
 RequestResult RoomMemberRequestHandler::handleRequest(RequestInfo r)
 {
+    m_room = m_roomManager.getRoom(m_room.getRoomData().id); //refresh the room
+
     return RequestResult(); //TODO
+}
+
+RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo r)
+{
+    GetRoomStateResponse response;
+    response.answerTimeout = m_room.getRoomData().timePerQuestion;
+    response.hasGameBegun = m_room.getRoomData().isActive == IN_GAME;
+    response.players = m_room.getAllUsers();
+    response.questionCount = m_room.getRoomData().numOfQuestionsInGame;
+    response.status = r.requestCode;
+    std::vector<unsigned char> buffer = JsonResponsePacketSerializer::serializeGetRoomStateResponse(response);
+    return IRequestHandler::createRequestResult(buffer, this);
 }
