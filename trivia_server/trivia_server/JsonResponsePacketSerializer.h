fe8105d23b44afcf5623d0c9cb81e02f3b44c52c
Eddet Moshe <edeet@themoshes.com>
2022-05-27 11:46:31 +0300

    Added v4 structs

--- trivia_server/trivia_server/JsonResponsePacketSerializer.h
+++ trivia_server/trivia_server/JsonResponsePacketSerializer.h
@@ -2,8 +2,17 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <map>
 #include "define.h"
 #include "Room.h"
+
+typedef struct PlayerResults
+{
+	std::string username;
+	unsigned int correctAnswerCount;
+	unsigned int wrongAnswerCount;
+	unsigned int averageAnserTime;
+}PlayerResults;
 
 typedef struct LoginResponse
 {
@@ -82,6 +91,30 @@ typedef struct LeaveRoomResponse
 {
 	unsigned int status;
 }LeaveRoomResponse;
+
+typedef struct LeaveGameResponse 
+{
+	unsigned int status;
+}LeaveGameResponse;
+
+typedef struct GetQuestionResponse 
+{
+	unsigned int status;
+	std::string question;
+	std::map<unsigned int, std::string> answers;
+}GetQuestionResponse;
+
+typedef struct SubmitAnswerResponse 
+{
+	unsigned int status;
+	unsigned int correctAnswerId;
+}SubmitAnswerResponse;
+
+typedef struct GetGameResultsResponse 
+{
+	unsigned int status;
+	std::vector<PlayerResults> results;
+}GetGameResultsResponse;
 
 
 class JsonResponsePacketSerializer
