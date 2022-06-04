4850fc761756546bfb2e2d5eb69b66001b92da56
Gali Yefet <yefetg1967@gmail.com>
2022-06-04 16:26:50 +0300

    finished checks

--- trivia_client/trivia_client/CreateRoomPage.xaml.cs
+++ trivia_client/trivia_client/CreateRoomPage.xaml.cs
@@ -53,7 +53,7 @@ namespace trivia_client
             byte[] msg = classes.Serializer.serializeCreateRoomRequest(r);
             msg = _connector.sendGetData(msg);
             classes.CreateRoomResponse response = classes.Deserializer.deserializeCreateRoomResponse(msg);
-
+            
             //check if creation failed and move to page accordingly
             if (response.status != classes.Deserializer.CREATE_ROOM_CODE)
             {
